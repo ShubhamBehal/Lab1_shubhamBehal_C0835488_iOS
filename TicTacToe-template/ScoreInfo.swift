a496237de4317752e7ace40e39ab91004c0a60a2
Shubham <shubh.behal76@gmail.com>
2022-01-18 18:07:02 -0500

    Initial Commit

---
+++ TicTacToe-template/ScoreInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreInfo.swift
+//  ScoreInfo
+//
+//  Created by Shubham Behal on 17/01/22.
+//  Copyright © 2022 mohammadkiani. All rights reserved.
+//
+
+import Foundation
+
+class ScoreInfo{
+    var isCross = false
+    var crossIds : [Int] = []
+    var noughtsIds : [Int] = []
+    var naughtScore = 0
+    var crossScore = 0
+    var drawCount = 0
+}
---
+++ TicTacToe-template/ViewController.swift
@@ -0,0 +1,197 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var btnPlayAgain: UIButton!
+    @IBOutlet weak var svBoard: UIStackView!
+    @IBOutlet weak var naughtScore: UILabel!
+    @IBOutlet weak var imgBoard: UIImageView!
+    @IBOutlet weak var crossScore: UILabel!
+    @IBOutlet weak var drawCount: UILabel!
+    
+
+    let scores = ScoreInfo()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        btnPlayAgain.isHidden = true
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        view.addGestureRecognizer(swipeRight)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe))
+        swipeUp.direction = UISwipeGestureRecognizer.Direction.up
+        view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe))
+        swipeDown.direction = UISwipeGestureRecognizer.Direction.down
+        view.addGestureRecognizer(swipeDown)
+    }
+
+    @IBAction func onPlayAgainClicked(_ sender: Any) {
+      restartGame()
+    }
+    
+    @IBAction func onBtn1Pressed(_ sender: Any) {
+        handleButtonPressed(btn : btn1)
+    }
+    
+    @IBAction func onBtn2Pressed(_ sender: Any) {
+        handleButtonPressed(btn : btn2)
+    }
+    
+    @IBAction func onBtn3Pressed(_ sender: Any) {
+        handleButtonPressed(btn : btn3)
+    }
+    
+    @IBAction func onBtn4Pressed(_ sender: Any) {
+        handleButtonPressed(btn : btn4)
+    }
+    
+    @IBAction func onBtn5Pressed(_ sender: Any) {
+        handleButtonPressed(btn : btn5)
+    }
+    
+    @IBAction func onBtn6Pressed(_ sender: Any) {
+        handleButtonPressed(btn : btn6)
+    }
+    
+    @IBAction func onBtn7Pressed(_ sender: Any) {
+        handleButtonPressed(btn : btn7)
+    }
+    
+    @IBAction func onBtn8Pressed(_ sender: Any) {
+        handleButtonPressed(btn : btn8)
+    }
+    
+    @IBAction func onBtn9Pressed(_ sender: Any) {
+        handleButtonPressed(btn : btn9)
+    }
+    
+    func handleButtonPressed(btn: UIButton){
+        
+        if(scores.crossIds.contains(btn.tag) ||
+           scores.noughtsIds.contains(btn.tag)){
+            return
+        }
+        
+        if((scores.crossIds.count + scores.noughtsIds.count) == 8){
+            svBoard.isHidden = true
+            imgBoard.isHidden = true
+            lblResult.isHidden = false
+            btnPlayAgain.isHidden = false
+            scores.drawCount += 1
+            lblResult.text = "Its a draw"
+            naughtScore.text = "Naught - \(scores.naughtScore)"
+            crossScore.text = "Cross - \(scores.crossScore)"
+            drawCount.text = "Draws - \(scores.drawCount)"
+            
+            resetValues()
+            return
+        }
+      
+        if(scores.isCross){
+            scores.crossIds.append(btn.tag)
+            btn.setImage(UIImage(named: "cross.png"), for: .normal)
+            checkForResult(selectedBtnIds: scores.crossIds , isCross :scores.isCross)
+            scores.isCross = false
+        }else{
+            scores.noughtsIds.append(btn.tag)
+            btn.setImage(UIImage(named: "nought.png"), for: .normal)
+            checkForResult(selectedBtnIds :scores.noughtsIds  , isCross :scores.isCross)
+            scores.isCross = true
+        }
+        btn.alpha = 0
+                UIView.animate(withDuration: 2) {
+                    btn.alpha = 1
+                }
+        
+    }
+    
+    func checkForResult(selectedBtnIds :[Int] , isCross : Bool){
+        if((selectedBtnIds.contains(btn1.tag) && selectedBtnIds.contains(btn2.tag) &&
+           selectedBtnIds.contains(btn3.tag)) ||
+           (selectedBtnIds.contains(btn4.tag) && selectedBtnIds.contains(btn5.tag) &&
+              selectedBtnIds.contains(btn6.tag)) ||
+           (selectedBtnIds.contains(btn7.tag) && selectedBtnIds.contains(btn8.tag) &&
+               selectedBtnIds.contains(btn9.tag)) ||
+           (selectedBtnIds.contains(btn1.tag) && selectedBtnIds.contains(btn4.tag) &&
+              selectedBtnIds.contains(btn7.tag)) ||
+              (selectedBtnIds.contains(btn2.tag) && selectedBtnIds.contains(btn5.tag) &&
+                 selectedBtnIds.contains(btn8.tag)) ||
+              (selectedBtnIds.contains(btn3.tag) && selectedBtnIds.contains(btn6.tag) &&
+                  selectedBtnIds.contains(btn9.tag)) ||
+           (selectedBtnIds.contains(btn1.tag) && selectedBtnIds.contains(btn5.tag) &&
+               selectedBtnIds.contains(btn9.tag)) ||
+           (selectedBtnIds.contains(btn3.tag) && selectedBtnIds.contains(btn5.tag) &&
+               selectedBtnIds.contains(btn7.tag))
+        ){
+            displayResult(isCross : isCross)
+        }
+    }
+    
+    func displayResult(isCross :Bool){
+        svBoard.isHidden = true
+        imgBoard.isHidden = true
+        lblResult.isHidden = false
+        btnPlayAgain.isHidden = false
+        if(isCross){
+            scores.crossScore += 1
+            lblResult.text = "Cross is the Winner"
+        }else{
+            scores.naughtScore += 1
+            lblResult.text = "Nought is the Winner"
+        }
+        naughtScore.text = "Naught - \(scores.naughtScore)"
+        crossScore.text = "Cross - \(scores.crossScore)"
+        
+        resetValues()
+    }
+    
+    func resetValues(){
+        scores.crossIds = []
+        scores.noughtsIds = []
+        scores.isCross = false
+        btn1.setImage(nil, for: .normal)
+        btn2.setImage(nil, for: .normal)
+        btn3.setImage(nil, for: .normal)
+        btn4.setImage(nil, for: .normal)
+        btn5.setImage(nil, for: .normal)
+        btn6.setImage(nil, for: .normal)
+        btn7.setImage(nil, for: .normal)
+        btn8.setImage(nil, for: .normal)
+        btn9.setImage(nil, for: .normal)
+    }
+    
+    func restartGame(){
+        btnPlayAgain.isHidden = true
+        lblResult.isHidden = true
+        svBoard.isHidden = false
+        imgBoard.isHidden = false
+    }
+    
+    @objc func onSwipe(gesture: UISwipeGestureRecognizer) {
+        resetValues()
+        restartGame()
+    }
+}
+
